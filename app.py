d6dfdbf01cd6025c72aba6b6a7877ed1be10c6ab
Marcos Klein <mklein0@gmail.com>
2022-08-21 15:12:02 -0700

    Integrate Flask Login to Auth Lib.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import typing as t_
 import time
 import json
 import os
 
 import certifi
 import flask
+import flask_login
 import requests
 
 from authlib.integrations.flask_client import OAuth, OAuthError
@@ -21,6 +23,7 @@ clientId = os.getenv('CLIENT_ID', 'flask-webapp')
 clientSecret = os.getenv('CLIENT_SECRET', 'lkkoQDUdJUqYDHXZBVDodw2ocvqJEflP')
 oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
 
+
 # We do not handle OAuth2 tokens separate from authentication server, so no need to hold them
 # beyond the flask.session.
 
@@ -30,7 +33,6 @@ oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
 #         refresh_token=None,
 #         access_token=None
 # ):
-#     import pdb; pdb.set_trace()
 #     if refresh_token:
 #         item = OAuth2Token.find(name=name, refresh_token=refresh_token)
 #     elif access_token:
@@ -44,70 +46,170 @@ oidcDiscoveryUrl = f'{issuer}/.well-known/openid-configuration'
 #     item.expires_at = token['expires_at']
 #     item.save()
 
-
-oauth = OAuth(
-    app=app,
-    # update_token=update_token,
-)
-oauth.register(
-    name='keycloak',
-    client_id=clientId,
-    client_secret=clientSecret,
-    server_metadata_url=oidcDiscoveryUrl,
-    client_kwargs={
-        'scope': 'openid email profile',
-        'code_challenge_method': 'S256'  # enable PKCE
-    },
-)
+def clear_session():
+    # type: (...) -> None
+    flask.session.pop('id_token', None)
+    flask.session.pop('oa_token', None)
 
 
-def fetch_token():
-    if 'authToken' not in flask.session:
-        return None
+class FlaskLoginAuthLibUser(flask_login.UserMixin):
+    def __init__(
+            self,
+            user_info  # type: dict
+    ):
+        # type: (...) -> None
+        """
+        :param user_info: ID Token following specification:
+            https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+        """
+        # Convert identifier of OAuth Identity to a unique id string excluding session/client info.
+        self.user_info = user_info
+        self.id = json.dumps(
+            {
+                'iss': user_info['iss'],
+                'sub': user_info['sub'],
+            },
+            sort_keys=True, indent=0
+        )
 
-    now = time.time()
-    authToken = flask.session['authToken']
-    # get current access token
-    # check if access token is still valid
-    # if current access token is valid, use token for request
-    # if current access token is invalid, use refresh token to obtain new access token
-    # if successfully, update current access token, current refresh token
-    # if current access token is valid, use token for request
+    @property
+    def access_token(self):
+        # type: (...) -> str
+        """
+        Login Manager should have validated access token, so just get it from the User Session.
+        """
+        return flask.session['oa_token']['access_token']
 
-    if authToken['expires_at'] - 2 < now:  # refresh 2 seconds before access token expires
-        try:
-            new_response = oauth.keycloak.fetch_access_token(
-                refresh_token=authToken['refresh_token'],
-                grant_type='refresh_token'
-            )
 
-        except OAuthError as e:
-            if e.error != 'invalid_grant':
-                raise
+class FlaskAuthLibLoginManager(flask_login.LoginManager):
 
-            clear_session()
+    def __init__(self, app=None, add_context_processor=True):
+        super().__init__(app, add_context_processor=add_context_processor)
+
+        self.oauth_service = OAuth(
+            app=app,
+            # update_token=update_token,
+        )
+        self.oauth_service.register(
+            name='keycloak',
+            client_id=clientId,
+            client_secret=clientSecret,
+            server_metadata_url=oidcDiscoveryUrl,
+            client_kwargs={
+                'scope': 'openid email profile',
+                'code_challenge_method': 'S256'  # enable PKCE
+            },
+        )
+        self.request_loader(self.keycloak_load_request)
+
+    def keycloak_load_request(
+            self,
+            request  # type: flask.Request
+    ):
+        # type: (...) -> t_.Optional[flask_login.UserMixin]
+        access_token = self.keycloak_fetch_token()
+        if access_token is None:
             return None
 
-        flask.session['authToken'].update(new_response)
-        # Mark flask.session tampered with.
-        flask.session.modified = flask.session.accessed = True
+        return FlaskLoginAuthLibUser(flask.session['id_token'])
 
-    access_token = authToken['access_token']
-    return access_token
+    def keycloak_fetch_token(self):
+        # type: (...) -> t_.Optional[str]
+        if 'oa_token' not in flask.session:
+            return None
+
+        now = time.time()
+        oa_token = flask.session['oa_token']
+        # get current access token
+        # check if access token is still valid
+        # if current access token is valid, use token for request
+        # if current access token is invalid, use refresh token to obtain new access token
+        # if successfully, update current access token, current refresh token
+        # if current access token is valid, use token for request
+
+        if oa_token['expires_at'] - 2 < now:  # refresh 2 seconds before access token expires
+            try:
+                refreshed_token = self.oauth_service.keycloak.fetch_access_token(
+                    refresh_token=oa_token['refresh_token'],
+                    grant_type='refresh_token'
+                )
+
+            except OAuthError as e:
+                if e.error != 'invalid_grant':
+                    raise
+
+                clear_session()
+                return None
+
+            id_token = self.oauth_service.keycloak.parse_id_token(refreshed_token)
+            flask.session['id_token'] = id_token
+            refreshed_token.pop('id_token', None)
+            oa_token = flask.session['oa_token'] = refreshed_token
+
+            # Mark flask.session tampered with.
+            flask.session.modified = flask.session.accessed = True
+
+        return oa_token['access_token']
+
+    def keycloak_authorize_access_token(self):
+        oa_token = self.oauth_service.keycloak.authorize_access_token()
+
+        # OAuth Token contains User Info encoded in JWT.  Should we drop it?
+        id_token = self.oauth_service.keycloak.parse_id_token(oa_token)
+        if id_token:
+            # https://openid.net/specs/openid-connect-core-1_0.html#IDToken
+            flask.session['id_token'] = id_token
+            # https://www.rfc-editor.org/rfc/rfc6749#section-4.1.4
+            new_token = dict(oa_token)
+            new_token.pop('id_token', None)
+            flask.session['oa_token'] = new_token
+
+        return oa_token
+
+    def keycloak_authorize_redirect(
+            self,
+            redirect_uri
+    ):
+        # type: (...) -> flask.BaseResponse
+        return self.oauth_service.keycloak.authorize_redirect(redirect_uri)
+
+    def keycloak_logout(self):
+        oa_token = flask.session.get('oa_token')
+        if oa_token is not None:
+            # propagate logout to Keycloak
+            refresh_token = oa_token['refresh_token']
+            end_session_endpoint = f'{issuer}/protocol/openid-connect/logout'
+
+            requests.post(
+                end_session_endpoint,
+                data={
+                    "client_id": clientId,
+                    "client_secret": clientSecret,
+                    "refresh_token": refresh_token,
+                })
+            # We ignore the response
+
+        clear_session()
+
+
+login_manager = FlaskAuthLibLoginManager(app)
 
 
 @app.route('/')
 def index():
-    user = flask.session.get('user')
-    prettyIdToken = None
-    prettyAuthToken = None
-    if user is not None:
-        prettyIdToken = json.dumps(user, sort_keys=True, indent=4)
-        prettyAuthToken = json.dumps(flask.session['authToken'], sort_keys=True, indent=4)
+    user = flask_login.current_user._get_current_object(
+    )  # type: t_.Union[FlaskLoginAuthLibUser, flask_login.AnonymousUserMixin]
+
+    pretty_id_token = None
+    pretty_oa_token = None
+    if user.is_authenticated:
+        pretty_id_token = json.dumps(user.user_info, sort_keys=True, indent=4)
+        pretty_oa_token = json.dumps(flask.session['oa_token'], sort_keys=True, indent=4)
+
     return flask.render_template(
         'index.html',
-        idToken=prettyIdToken,
-        authToken=prettyAuthToken,
+        id_token=pretty_id_token,
+        oa_token=pretty_oa_token,
         now=int(time.time()),
     )
 
@@ -115,65 +217,36 @@ def index():
 @app.route('/login')
 def login():
     redirect_uri = flask.url_for('auth', _external=True)
-    return oauth.keycloak.authorize_redirect(redirect_uri)
+    return login_manager.keycloak_authorize_redirect(redirect_uri)
 
 
 @app.route('/auth')
 def auth():
-    authToken = oauth.keycloak.authorize_access_token()
-
-    # userinfo = oauth.keycloak.userinfo(request)
-    idToken = oauth.keycloak.parse_id_token(authToken)
-    if idToken:
-        flask.session['user'] = idToken
-        flask.session['authToken'] = authToken
+    login_manager.keycloak_authorize_access_token()
 
     return flask.redirect('/')
 
 
 @app.route('/api')
+@flask_login.login_required
 def api():
     """
     Use logged in user's access token to query user info
     """
-    access_token = fetch_token()
-    if access_token is None:
-        # User is not authenticated
-        raise flask.abort(401)
+    access_token = flask_login.current_user.access_token
 
-    userInfoEndpoint = f'{issuer}/protocol/openid-connect/userinfo'
-    userInfoResponse = requests.post(
-        userInfoEndpoint,
+    user_info_endpoint = f'{issuer}/protocol/openid-connect/userinfo'
+    user_info_response = requests.post(
+        user_info_endpoint,
         headers={'Authorization': f'Bearer {access_token}', 'Accept': 'application/json'}
     )
 
-    return userInfoResponse.text, 200
-
-
-def clear_session():
-    # type: (...) -> None
-    flask.session.pop('user', None)
-    flask.session.pop('authToken', None)
+    return user_info_response.text, 200
 
 
 @app.route('/logout')
 def logout():
-    authToken = flask.session.get('authToken')
-
-    if authToken is not None:
-        # propagate logout to Keycloak
-        refreshToken = authToken['refresh_token']
-        endSessionEndpoint = f'{issuer}/protocol/openid-connect/logout'
-
-        requests.post(
-            endSessionEndpoint,
-            data={
-                "client_id": clientId,
-                "client_secret": clientSecret,
-                "refresh_token": refreshToken,
-            })
-
-    clear_session()
+    login_manager.keycloak_logout()
     return flask.redirect('/')
 
 
